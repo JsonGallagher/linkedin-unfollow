6d8eca99ccf40896ec32cbb5d08b0b7c43c6e9f9
Jason Gallagher <jasong13@gmail.com>
2020-01-08 08:19:59 -0700

    Update LinkedIn Unfollow Script.js

--- LinkedIn Unfollow Script.js
+++ LinkedIn Unfollow Script.js
@@ -1,23 +1,22 @@
 // Refactored - Source: https://github.com/TalEliyahu/LinkedIn-Unfollower/blob/master/README.md
-
 let totalUnfollowed = 0;
 const unfollowFunction = setInterval(function() {
 const unfollowButton = document.querySelectorAll("ul > li [data-control-name=\"actor_follow_toggle\"].is-following");
     for(let i = unfollowButton.length - 1; i>=0; i--) {
      totalUnfollowed++;
-     UnfollowButton[i].click();
+     unfollowButton[i].click();
     }
    }, 1000);
 
-const ScrollToBottom = setInterval(function() {
+const scrollToBottom = setInterval(function() {
     window.scrollBy(0, 12.5);
    }, 15);
 
-function checkPageEnd(LastHeight) {
+function checkPageEnd(lastHeight) {
 let currentHeight = document.body.scrollHeight;
-    if(!(currentHeight > LastHeight)) {
-     clearInterval(ScrollToBottom);
-     clearInterval(UnfollowFunction);
+    if(!(currentHeight > lastHeight)) {
+     clearInterval(scrollToBottom);
+     clearInterval(unfollowFunction);
      alert(`Unfollowed ${totalUnfollowed} users`);
     } else setTimeout(function() {
         checkPageEnd(currentHeight);
